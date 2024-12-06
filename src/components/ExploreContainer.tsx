b52a62ae3f0cf0039f44245e3a3d2f35d507df3b
rtwohey <rtwohey@ionic.io>
2024-12-06 10:44:42 -0500

    Added ios & android, added camera and barcode scanner plugins

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,43 @@
+import { IonButton } from '@ionic/react';
 import './ExploreContainer.css';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { CapacitorBarcodeScanner, CapacitorBarcodeScannerOptions } from '@capacitor/barcode-scanner';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+
+  const takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+  
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    var imageUrl = image.webPath;
+  
+    // Can be set to the src of an image now
+    const imageElement = document.createElement('img');
+    if (imageUrl) {
+      imageElement.src = imageUrl;
+    }
+  }
+
+  const barcodeScanner = async () => {
+    const options: CapacitorBarcodeScannerOptions = {
+      hint: 0
+    };
+    const res = await CapacitorBarcodeScanner.scanBarcode(options);
+  }
+
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonButton onClick={takePicture}>Take Picture</IonButton>
+      <IonButton onClick={barcodeScanner}>Scan Barcode</IonButton>
     </div>
   );
 };
